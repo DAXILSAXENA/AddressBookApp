59a28bbc8b8537fa95f685e65b5c957ac6dbb039
DAXILSAXENA <daxildaxil@gmail.com>
2021-08-17 01:23:34 +0530

    [Daxil] Added:Spring Boot Address Book Application

--- src/main/java/com/bridgelabz/addressbookappdevelopment/entity/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbookappdevelopment/entity/AddressBook.java
@@ -1,6 +1,8 @@
+/** AddressBook.java */
 package com.bridgelabz.addressbookappdevelopment.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 
 import javax.persistence.Entity;
@@ -10,22 +12,27 @@ import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Entity
-@Getter
+@Data
 @AllArgsConstructor
 public class AddressBook {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
-    private String name;
+    private String firstName;
+    private String lastName;
     private String address;
-    private String email;
-    private String phoneNumber;
+    private String city;
     private String state;
+    private String zip;
+    private String phoneNumber;
+    private String email;
     private LocalDate createdDate;
     private LocalDate updatedDate;
 
     public AddressBook() {
-
+        this.createdDate = LocalDate.now();
+        this.updatedDate = LocalDate.now();
     }
 }
+
